48d30a4f9d482df9764d133950e98894317c31d3
Silas Wrinker <alcosojr79@gmail.com>
2024-06-15 17:32:43 -0400

    Update sand.h
    Add #define statements and more functions

--- sand.h
+++ sand.h
@@ -1,9 +1,22 @@
 #include <stdio.h>
-void greet();
+#include <stdlib.h>
+#define MAXUSER 40
+#define MAXPASS 100
+#define MINUSER 5
+#define MINPASS 5
+#define SESSION 10
+#define EOL '\n'
+
+char user[MAXUSER+1];
+char pass[MAXPASS+1];
+char session[SESSION+1];
+
+void greet(void);
+void login(char[], char[], char[]);
 void gline(int, int, char[]);
-void boxread();
-void boxcreate();
-void iread();
-void iwrite();
-void serve(port);
-void client(port);
+void boxread(void);
+void boxcreate(void);
+void iread(void);
+void iwrite(void);
+void serve(char[]);
+void client(char[]);
