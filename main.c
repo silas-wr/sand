14edc4bbb98c5d78f40d90f3629df6a2b3cf3dfc
Silas Wrinker <alcosojr79@gmail.com>
2024-06-15 17:34:25 -0400

    Update main.c
    add greeting

--- main.c
+++ main.c
@@ -1,30 +1,14 @@
-#include <stdio.h>
-#include <stdlib.h>
-#define MAXUSER 40
-#define MAXPASS 100
-#define MINUSER 5
-#define MINPASS 5
-#define SESSION 10
-#define EOL '\n'
+#include "sand.h"
 
-void gline(int, int, char[]);
+extern char user[MAXUSER+1];
+extern char pass[MAXPASS+1];
+extern char session[SESSION+1];
 
 main()
 {
-        char user[MAXUSER+1];
-	char pass[MAXPASS+1];
-	char session[SESSION+1];
-
-	printf("Username: ");
-	gline(MAXUSER, MINUSER, user);
-
-	printf("Password: ");
-	gline(MAXPASS, MINPASS, pass);
-
-	printf("Session code: ");
-	gline(SESSION, SESSION, session);
-
-	return 0;
+	greet();
+	login(user, pass, session);
+        return 0;
 }
 
 void gline(int max, int min, char s[])
@@ -40,12 +24,28 @@ void gline(int max, int min, char s[])
 	}
 
 	if (max != min && (i > max || i < min)) 
-		printf("[!] Invalid input (>=%i <=%i chars)\n", min, max);
+		printf("[!] Invalid input (>=%i <=%i chars)\n", min, max); exit(1);
 	else if (max == min && i != min)
-		printf("[!] Invalid input (=%i chars)\n", min);          	
+		printf("[!] Invalid input (=%i chars)\n", min); exit(1);
 	else
 		s[i] = '\0';
 
 	// printf("[i] %i chars received.\n", i);
 
+}
+
+void greet()
+{
+	printf("\033[2;36m  ________________________________\033[0;0m");
+	printf("\033[2;36m /|             \033[0;0m|/*\033[2;36m              /|\033[0;0m");
+	printf("\033[2;36m----------------\033[0;0m|\033[2;36m---------------/ |\033[0;0m");
+	printf("\033[2;36m|              \033[0;0m/|              \033[2;36m|  |\033[0;0m");
+	printf("\033[2;36m|             \033[0;0m! |              \033[2;36m| /|\033[0;0m");
+	printf("\033[2;36m|\033[2;33m==============\033[0;0m\\|\033[2;33m==============\033[2;36m|/ |\033[0;0m");
+	printf("\033[2;36m|               \033[0;0m|\\             \033[2;36m| /|\033[0;0m");
+	printf("\033[2;36m|               \033[0;0m| !\033[2;36m            |/ |\033[0;0m");
+	printf("\033[2;36m|\033[2;33m===============\033[0;0m|/\033[2;33m=============\033[2;36m|  |\033[0;0m");
+	printf("\033[2;36m|              \033[0;0m/|\033[2;36m              | /\033[0;0m");
+	printf("\033[2;36m|______________________________|/\033[0;0m");
+	printf( "\n The \033[2;32mSAND\033[0;0m shell by Silas Wrinker :: updated \033[2;32m2024\033[0;0m\n");
 }
