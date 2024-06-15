dd6e6e4f9e76db3f88bcdcf7f6ae9c14c5d8aa17
Silas Wrinker <alcosojr79@gmail.com>
2024-06-15 17:32:19 -0400

    Create users.c
    login function

---
+++ users.c
@@ -0,0 +1,13 @@
+#include "sand.h"
+
+void login(u, p, s)
+{
+  printf("Username: ");
+	gline(MAXUSER, MINUSER, u);
+
+	printf("Password: ");
+	gline(MAXPASS, MINPASS, p);
+
+	printf("Session code: ");
+	gline(SESSION, SESSION, s);  
+}
