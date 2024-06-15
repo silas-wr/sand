4dce35c2cef4fff4435478dd2aa02a586c12ef41
Silas Wrinker <alcosojr79@gmail.com>
2024-06-15 17:44:57 -0400

    Update users.c

--- users.c
+++ users.c
@@ -2,12 +2,12 @@
 
 void login(u, p, s)
 {
-  printf("Username: ");
-	gline(MAXUSER, MINUSER, u);
+	printf("Username: ");
+	gline(MAXUSER, MINUSER, &u);
 
 	printf("Password: ");
-	gline(MAXPASS, MINPASS, p);
+	gline(MAXPASS, MINPASS, &p);
 
 	printf("Session code: ");
-	gline(SESSION, SESSION, s);  
+	gline(SESSION, SESSION, &s);  
 }
