bae06c931f6a175d0d40b2ead757528c3c4b6855
Silas Wrinker <alcosojr79@gmail.com>
2024-06-15 20:02:07 -0400

    Update users.c
    moved gline func

--- core/users.c
+++ core/users.c
@@ -1,5 +1,28 @@
 #include "sand.h"
 
+void gline(int max, int min, char s[])
+{
+	int  i = 0;
+	char c = getchar();
+
+	while (c != EOL)
+	{
+		s[i] = c;
+		c = getchar();
+		++i;
+	}
+
+	if (max != min && (i > max || i < min)) 
+		printf("[!] Invalid input (>=%i <=%i chars)\n", min, max); exit(1);
+	else if (max == min && i != min)
+		printf("[!] Invalid input (=%i chars)\n", min); exit(1);
+	else
+		s[i] = '\0';
+
+	// printf("[i] %i chars received.\n", i);
+
+}
+
 void login(u, p, s)
 {
 	printf("Username: ");
