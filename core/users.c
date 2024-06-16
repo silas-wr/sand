340b9f7fb43392f5afcef10cf5ddd69dbacbefd2
Silas Wrinker <alcosojr79@gmail.com>
2024-06-15 20:04:20 -0400

    Update users.c

--- core/users.c
+++ core/users.c
@@ -13,11 +13,17 @@ void gline(int max, int min, char s[])
 	}
 
 	if (max != min && (i > max || i < min)) 
-		printf("[!] Invalid input (>=%i <=%i chars)\n", min, max); exit(1);
-	else if (max == min && i != min)
-		printf("[!] Invalid input (=%i chars)\n", min); exit(1);
-	else
+	{
+		printf("[!] Invalid input (>=%i <=%i chars)\n", min, max);
+		exit(1);
+	} else if (max == min && i != min)
+	{
+		printf("[!] Invalid input (=%i chars)\n", min);
+		exit(1);
+	} else
+	{
 		s[i] = '\0';
+	}
 
 	// printf("[i] %i chars received.\n", i);
 
